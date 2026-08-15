794111c4f0cf6e3abb367f4321711d381574a934
Joachim Byrnes-Shay <me@joachimbyrnesshay.com>
2026-08-14 23:58:22 -0500

    Add getComputerChoice to create valid turn value

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
-console.log("in scripts.js")
+
+/* create function getComputerChoice that randomly returns one of the following values: rock , paper, scissors. do it without an array just because*/
+
+// generate a random floating number between 0 and 1 (exclusive)
+// multiply * 3
+// calculate the floor of the result, now resulting in the number 0, 1, or 2 
+// link each of these values to one of the valid game values  rock, paper, scissors
+// return the specific game value assigned to the number returned by the earlier calculations 
+
+function getComputerChoice() {
+    const LIMIT = 3;
+    const index = Math.floor(Math.random() * LIMIT);
+    switch(index) {
+        case 0:
+            return "rock"
+        break;
+        case 1:
+            return "paper"
+        break;
+        case 2:
+            return "scissors"
+        break;
+        default:
+        break;
+    }
+}
+
+
+console.log(getComputerChoice());
